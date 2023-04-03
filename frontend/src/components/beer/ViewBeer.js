7f67b24d4c746c7c6946ae07fc7912eadabfd63a
Andrei_Varga <vargandr@yahoo.com>
2023-04-03 14:18:02 +0300

    w

--- frontend/src/components/beer/ViewBeer.js
+++ frontend/src/components/beer/ViewBeer.js
@@ -24,6 +24,7 @@ export default function ViewBeer() {
 
     const loadBeer=async()=>{
         const result = await axios.get(`api/beers/${beerId}`);
+        console.log(result);
         setBeer(result.data);
     }
 
