076ea738d285008106cfeafadcec9a7a2ec268b2
Andrei_Varga <vargandr@yahoo.com>
2023-04-23 13:49:31 +0300

    w

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,6 +7,7 @@ export default function Navbar(){
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="/Beer">Beer List</Link>
+                    <Link className="navbar-brand" to="/Producer">Producer List</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
