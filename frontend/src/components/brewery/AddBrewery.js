3997204b89fc3497f68ec41827ea60136d670520
Andrei_Varga <vargandr@yahoo.com>
2023-05-05 20:56:38 +0300

    w

---
+++ frontend/src/components/brewery/AddBrewery.js
@@ -0,0 +1,111 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
+export default function EditBrewery() {
+
+    let navigate = useNavigate();
+
+    const {id}= useParams();
+
+    const [brewery,setBrewery] = useState({
+        id:0,
+        name:"",
+        location:"",
+        year:0,
+        descr:"",
+        website:0
+    });
+
+    const{name,location,year,descr,website}=brewery;
+
+    const onInputChange = (e) => {
+        setBrewery({ ...brewery, [e.target.name]: e.target.value });
+    };
+
+    useEffect(() => {
+        loadBrewery();
+    }, []);
+
+    const onSubmit =async (e) => {
+        e.preventDefault();
+        await axios.put(`http://16.16.146.242:80/breweries/${id}`, brewery);
+        navigate("/");
+    };
+
+    const loadBrewery = async () => {
+        const result = await axios.get(`http://16.16.146.242:80/breweries/${id}`);
+        setBrewery(result.data);
+    }
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 shadow offset-md-3 border rounded mx-auto p-4 mt-2">
+                    <h2 className="text-center m-4">Edit Producer</h2>
+
+                    <form onSubmit={(e)=>onSubmit(e)}>
+                        <div className="mb-3">
+                            <label>Name</label>
+                            <input
+                                type = {"text"}
+                                className={"form-control"}
+                                placeholder={"Enter name"}
+                                name={"name"}
+                                value={name}
+                                onChange={(e)=>onInputChange(e)}
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label>Location</label>
+                            <input
+                                type = {"text"}
+                                className={"form-control"}
+                                placeholder={"Enter location"}
+                                name={"location"}
+                                value={location}
+                                onChange={(e)=>onInputChange(e)}
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label>Founding year</label>
+                            <input
+                                type = {"number"}
+                                className={"form-control"}
+                                placeholder={"Enter founding year"}
+                                name={"year"}
+                                value={year}
+                                onChange={(e)=>onInputChange(e)}
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label>Description</label>
+                            <input
+                                type = {"text"}
+                                className={"form-control"}
+                                placeholder={"Enter description"}
+                                name={"descr"}
+                                value={descr}
+                                onChange={(e)=>onInputChange(e)}
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label>Website</label>
+                            <input
+                                type = {"number"}
+                                className={"form-control"}
+                                placeholder={"Enter website"}
+                                name={"website"}
+                                value={website}
+                                onChange={(e)=>onInputChange(e)}
+                            />
+                        </div>
+                        <button type="submit" className="btn btn-outline-primary">Submit</button>
+                        <Link  className="btn btn-outline-danger mx-2" to="/">Cancel</Link>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ frontend/src/components/brewery/ViewBrewery.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+
+export default function ViewBrewery() {
+
+    const [brewery,setBrewery] = useState({
+        id:0,
+        name:"",
+        location:"",
+        year:0,
+        descr:"",
+        website:0,
+        beerBreweries:[]
+    });
+
+    const {id}=useParams();
+
+    useEffect(()=>{
+        loadBrewery();
+
+    },[])
+
+    const loadBrewery=async()=>{
+        const result = await axios.get(`http://16.16.146.242:80/breweries/${id}`);
+        console.log(result);
+        setBrewery(result.data);
+    }
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 shadow offset-md-3 border rounded mx-auto p-4 mt-2">
+                    <h2 className="text-center m-4">Brewery Details</h2>
+
+                    <div className="card">
+                        <div className="card-header">
+                            Details of  brewery {brewery.id}:
+                            <ul className="list-group list-group-flush">
+
+                                <li className="list-group-item">
+                                    <b>Name:</b>
+                                    {brewery.name}
+                                </li>
+                                <li className="list-group-item">
+                                    <b>Location:</b>
+                                    {brewery.location}
+                                </li>
+                                <li className="list-group-item">
+                                    <b>Founding year:</b>
+                                    {brewery.year}
+                                </li>
+                                <li className="list-group-item">
+                                    <b>Description:</b>
+                                    {brewery.descr}
+                                </li>
+                                <li className="list-group-item">
+                                    <b>Website:</b>
+                                    {String(brewery.website)}
+                                </li>
+                                <li className="list-group-item">
+                                    <b>Beers:</b>
+                                    {beer.beerBreweries?.map(beerBrewery => (
+                                        <div>
+                                            <br/>Brewery {beerBrewery?.brewery.id}<br/>
+                                            Quantity: {beerBrewery?.quantity}<br/>
+                                            Tested: {beerBrewery?.tested.toString()}
+                                        </div>
+                                    ))}
+                                </li>
+                            </ul>
+
+                        </div>
+                    </div>
+                    <Link className="btn btn-primary my-2" to="/">Back to Home</Link>
+                </div>
+            </div>
+
+        </div>
+
+    )
+}
