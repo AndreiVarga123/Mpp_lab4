bcee0f4eb39d9913ebf7c99a97e566602f233302
Andrei_Varga <vargandr@yahoo.com>
2023-05-08 08:45:55 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import PreApp from "./PreApp";
 const root = ReactDOM.createRoot(document.getElementById('root')) ;
 root.render(
   <React.StrictMode>
-    <PreApp/>
+    <App/>
   </React.StrictMode>
 );
 
